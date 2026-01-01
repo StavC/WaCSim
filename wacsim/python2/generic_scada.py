02f443f7a25b36ce887118e77ecf4acb13d2581c
Stav Cohn <stav@wacsim.dev>
2026-01-01 17:55:15 +0200

    Fix indentation errors in generic_plc.py and generic_scada.py, initialize water_loss_df early, set debug mode

--- wacsim/python2/generic_scada.py
+++ wacsim/python2/generic_scada.py
@@ -432,7 +432,7 @@ class GenericScada(BasePLC):
                         # This prevents None from overwriting valid commands when multiple
                         # controls target the same actuator (e.g., BELOW and ABOVE rules)
                         if decision is not None:
-                        ControlsActions.append((control.actuator, decision))
+                            ControlsActions.append((control.actuator, decision))
                     else:
                         # If custom algorithm should decide, avoid repeated control of same actuator
                         if control.actuator in SkipNextActuatorList:
@@ -440,11 +440,11 @@ class GenericScada(BasePLC):
 
                         # Load custom algorithm module (cached for performance)
                         if HybridAction not in self._module_cache:
-                        ScriptName = HybridAction.split('/')[-1]
-                        spec = importlib.util.spec_from_file_location(ScriptName, HybridAction)
-                        module = importlib.util.module_from_spec(spec)
-                        sys.modules[ScriptName] = module
-                        spec.loader.exec_module(module)
+                            ScriptName = HybridAction.split('/')[-1]
+                            spec = importlib.util.spec_from_file_location(ScriptName, HybridAction)
+                            module = importlib.util.module_from_spec(spec)
+                            sys.modules[ScriptName] = module
+                            spec.loader.exec_module(module)
                             self._module_cache[HybridAction] = module
                         else:
                             module = self._module_cache[HybridAction]
